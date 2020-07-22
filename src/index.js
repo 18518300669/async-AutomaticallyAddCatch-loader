5f6621460d73a6c081a328f97ec1fb419e06bb90
Dylan <Dylan.zhu@zenlayer.com>
2020-07-22 18:11:54 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const babel = require('@babel/core');
 const loaderUtils = require('loader-utils');
 module.exports = function (source) {
-  let options = loaderUtils.getOptions(this);
+  let options = loaderUtils.getOptions(this) || {};
   const { code } = babel.transformSync(source, {
     presets: [
       ['@babel/preset-typescript', { allExtensions: true, isTSX: true }],
